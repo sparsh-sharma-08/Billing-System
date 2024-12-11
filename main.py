d7476804070823d9eab4f28b0b3316a5eb4c6cfd
Sparsh Sharma <sparshsharma0825@gmail.com>
2024-12-11 23:35:00 +0530

    Final Changes Made

--- main.py
+++ main.py
@@ -76,13 +76,40 @@ def delete_item(lists):
         print("Invalid input! Please enter a valid number!")
 
 def bill_calc(lists):
-    pass
+    if not lists:
+        print("Cart is empty. Nothing to calculate!")
+        return
+    print("\nGenerating Bill....")
+    print("_"*40)
+    total=0
+    for index, item in enumerate(lists, start=1):
+        print(f"{index}. {item['Item name']}: ₹{item['Item price']:.2f}")
+        total+=item['Item price']
+    print("_"*40)
+    print(f"Total Bill: {total:.2f}")
+    print("_"*40)
 
+    save_bill=input("Do you want to save this bill? (y/n)").lower()
+    if save_bill=='y':
+        with open("generated_bill.txt","w")as bill_file:
+            bill_file.write("Generated Bill\n")
+            bill_file.write("_"*40+"\n")
+            for index, item in enumerate(lists, start=1):
+                bill_file.write(f"{index}. {item['Item name']}: {item['Item price']:.2f}\n")
+            bill_file.write("_"*40 +"\n")
+            bill_file.write(f"Total Bill: {total:.2f}\n")
+            bill_file.write("_"*40+"\n")
+        print("Bill saved as 'generated_bill.txt'")
 def main():
     lists = list_load()
     while True:
         print("Welcome to Billing Software!")
-        print("1. Add Item\n2. List Cart Details\3. Update Card\n4. Delete Item\n5. Calculate Bill\n6. Exit")
+        print("1. Add Item")
+        print("2. List Cart Details")
+        print("3. Update Card")
+        print("4. Delete Item")
+        print("5. Calculate Bill")
+        print("6. Exit")
         user_input=input("Enter the operation no.: ")
         match user_input:
             case "1":
